d1afcb9ed40d4ae647691de901da9ffe620a1f87
AjayBabuGorantla <ajaybabu@pdx.edu>
2022-06-05 16:30:30 -0700

    added contact me form functionality and made it responsive

--- js/script.js
+++ js/script.js
@@ -11,3 +11,73 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     })
 })
+
+document.querySelector(".contact").addEventListener('submit', function(event){
+    let inputName = document.getElementById("name").value
+    let inputEmail = document.getElementById("email").value
+    let inputMessage = document.getElementById("message").value
+    // console.log(inputName)
+    var regExpression = /\S+@\S+\.\S+/
+    
+    if((inputName==="")||(inputEmail==="")||(inputMessage==="")){
+        console.log("Please enter all details!")
+
+        const alertPlaceHolder = document.getElementById("alertPlaceHolder")
+
+        console.log(alertPlaceHolder)
+
+        const alert = (message, type) => {
+            const wrapper = document.createElement('div')
+            wrapper.innerHTML = [
+              `<div class="alert alert-danger alert-dismissible" role="alert">`,
+              `   <div>${message}</div>`,
+              '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+              '</div>'
+            ].join('')
+          
+            alertPlaceHolder.append(wrapper)
+          }
+
+        alert('Please enter all the details. Drop a "Hi!" and your contact number if you have a time crunch. I will reach out to you at the earliest :)', 'success')
+        event.preventDefault()
+    }
+
+    else if(regExpression.test(inputEmail)!=true){
+        console.log("Please enter a valid email address")
+    }
+
+    else{
+        inputName=""
+        inputEmail=""
+        inputMessage=""
+
+        const alertPlaceHolder = document.getElementById("alertPlaceHolder")
+
+        console.log(alertPlaceHolder)
+
+        const alert = (message, type) => {
+            const wrapper = document.createElement('div')
+            wrapper.innerHTML = [
+              `<div class="alert alert-success alert-dismissible" role="alert">`,
+              `   <div>${message}</div>`,
+              '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+              '</div>'
+            ].join('')
+          
+            alertPlaceHolder.append(wrapper)
+          }
+
+        alert('Thank you! Your message has been received. Will reach out to you at the earliest :)', 'success')
+        event.preventDefault()
+    }
+})
+
+document.querySelector(".contact").addEventListener('reset', function(){
+    let inputName = document.getElementById("name").value
+    let inputEmail = document.getElementById("email").value
+    let inputMessage = document.getElementById("message").value
+
+    inputName=""
+    inputEmail=""
+    inputMessage=""
+})
